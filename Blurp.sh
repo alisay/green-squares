2843e4b8c82c1a1abcf317c96c0723f1d76723af
alisa <alisa.blakeney@gmail.com>
2024-11-13 13:13:29 +0800

    bloop

---
+++ Blurp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# green squares maketh the programmer
+
+adjust() {
+    NEWDATE=$(gdate -d "$1")
+	echo "$RANDOM" | tee bleeep
+	git add bleeep
+	git commit -am "$NEWDATE"
+    GIT_COMMITTER_DATE="$NEWDATE" git commit --amend --no-edit --date "$NEWDATE"
+    PREVDATE=$(gdate -d "$1" +%Y-%m-%d)
+}
+
+adjust "26 October 2020"
+
+for i in $(seq 0 1789); do
+	echo $i
+	adjust "$PREVDATE + 1 days"
+done
